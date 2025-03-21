d75b7694eab60297536999f4e65b1a8c4581c91d
小林 <keigo21emon@gmail.com>
2025-03-22 01:49:17 +0900

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,25 +1,37 @@
-int IN1 = 2;
-int Pin1 = A0;
-float value1 = 0;
+const int IN1 = 2;    //ポンプの制御ピン
+const int Pin1 = A0;  //湿度センサのピン
+const int THRESHOLD = 470; //湿度のしきい値
+const int READ_TIMES = 5;  //センサー値を読み取り回数
 
 void setup() {
   Serial.begin(9600);
   pinMode(IN1, OUTPUT);
   pinMode(Pin1, INPUT);
+  
+  //初期状態でポンプをOFFにしておく
   digitalWrite(IN1, LOW);
-  delay(500);
 }
 
 void loop() {
-  Serial.print("MOISTURE LEVEL:");
-  value1 = analogRead(Pin1);
-  Serial.println(value1);
-  if (value1 > 470) {
-    digitalWrite(IN1, LOW);
-  } else {
-    digitalWrite(IN1, HIGH);
+  float value1 = 0;
+  
+  for (int i = 0; i < READ_TIMES; i++) {
+    value1 += analogRead(Pin1);
+    delay(10);  //10ms待機
   }
+  value1 /= READ_TIMES;
+
+  //湿度数の出力
+  Serial.print("MOISTURE LEVEL: ");
+  Serial.println(value1);
+
+  //値に応じてポンプを制御
+  if (value1 > THRESHOLD) {
+    digitalWrite(IN1, LOW);  //OFF
+  } else {
+    digitalWrite(IN1, HIGH); //ON
+  }
+
   Serial.println();
-  delay(1000);
+  delay(1000); // 1秒ごとに測定
 }
-このコードを添削して
